699b1590536833a8555ab0a7c8ba642894a3d585
Maik Lathan <maik.lathan@gmail.com>
2022-04-25 16:14:26 +0200

    Fixed issue that multiple forward slashes where not handle correctly in that regular expression also removed important slashes.

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,8 +1,11 @@
 function render_download_url(baseurl, version, platform, withTutorial) {
+	if (baseurl.trim().endsWith('/') === false) {
+		baseurl = baseurl + '/';
+	}
 	var url = withTutorial ? 
-	`${baseurl}/v${version}/redmineBOOST_v${version}_with-tutorial_${platform}.zip` :
-	`${baseurl}/v${version}/redmineBOOST_v${version}_${platform}.zip`;
-	return url.replace(/\/+/g, '/')
+	`${baseurl}v${version}/redmineBOOST_v${version}_with-tutorial_${platform}.zip` :
+	`${baseurl}v${version}/redmineBOOST_v${version}_${platform}.zip`;
+	return url;
 }
 
 function set_download_urls(withTutorial) {
