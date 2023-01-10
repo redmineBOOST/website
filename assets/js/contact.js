480c68aaa992fa5edd4647ff88e959d3e56b31f9
Maik Lathan <maik.lathan@hicknhack-software.com>
2023-01-10 15:04:12 +0100

    Added WebForms Service to contact form.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -13,34 +13,6 @@ function update_github_link() {
     };
 }
 
-function update_mail_link() {
-    const message_input = document.getElementById('mail-contact-input-message');
-    const message_input_error_description = document.getElementById('mail-contact-input-message-error-description');
-    var mail_contact_button = document.getElementById('mail-contact-submit');
-    mail_contact_button.onclick = function () {
-        var valid = is_contact_form_valid(message_input, message_input_error_description);
-        if (valid) {
-            var mailto_location = parse_mailto_link(message_input);
-            window.location.href = mailto_location;
-        }
-    };
-}
-
-function is_contact_form_valid(message_input, error_desc) {
-    var valid = true;
-    /* message is required */
-    if (message_input.value == '') {
-        message_input.classList.add('error');
-        error_desc.classList.add('error');
-        valid = false;
-    }
-    else {
-        message_input.classList.remove('error');
-        error_desc.classList.remove('error');
-    }
-    return valid;
-}
-
 function is_checkbox_accepted(checkbox_input, error_desc) {
     var valid = true;
     /* checkbox is required */
@@ -61,12 +33,6 @@ function parse_github_link(message_input) {
     return 'https://github.com/redmineBOOST/support/issues/new?labels=question&body=' + message;
 }
 
-function parse_mailto_link(message_input) {
-    var message = message_input.value.replace(/(?:\r\n|\r|\n)/g, '%0D%0A');;
-    var subject = '[redmineBOOST]'
-    return 'mailto:redmineboost@hicknhack-software.com?subject=' + subject + '&body=' + message;
-}
-
 /* main */
 document.addEventListener('DOMContentLoaded', function () {
     var github_contact_form = document.getElementById('github-contact-form');
@@ -75,9 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     update_github_link();
 
-    var mail_contact_form = document.getElementById('mail-contact-form');
-    mail_contact_form.addEventListener('change', function () {
-        update_mail_link();
-    });
-    update_mail_link();
+    const form = document.getElementById('mail-contact-form');
+    HnH.WebForms.bindForm(form);
 });
